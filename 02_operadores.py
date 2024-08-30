26b45e5b44ff182454ef0891775e2f486f05f9b4
Yensi Jesús González Nova <yensigonzalesnova55@gmail.com>
2024-08-30 13:48:20 +0200

    viendo operadores

---
+++ 02_operadores.py
@@ -0,0 +1,34 @@
+### Operadores ###
+
+print(3 + 4); 
+print(3 - 4); 
+print(3 * 4); 
+print(3 / 4); 
+print(10 % 3); 
+print(10 // 3); 
+print(2 ** 3); 
+
+print("Hola" + "Python " + "¿Qué tal?")
+# print("Hola" - "Python"); error 
+print("Hola " + str(5))
+#print("Hola " * 5)
+#print("Hola " * str(5))
+
+my_float = 2.5 * 2
+#print("Hola " + int(my_float))
+
+### Operadores Comparativos ###
+print(3 > 4)
+print(3 < 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("Hola" > "Python")
+print("Hola" < "Python")
+print(len("aaaa") >= len("abaaa")) # cuenta caracteres
+print("Hola" <= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
+print(not(3 > 4))
