9031109b92472cede135690e02aebbe4fe7f9c7c
Yensi Jesús González Nova <yensigonzalesnova55@gmail.com>
2024-08-30 14:40:10 +0200

    Strings

---
+++ 03_strings.py
@@ -0,0 +1,52 @@
+### Strings ###
+
+my_string = "Mi String"
+my_other_string = "Mi otro String"
+
+print(len(my_string))
+print(len(my_other_string))
+
+print( my_string + " " + my_other_string)
+
+my_new_line_string = "Este es un String\ncon salto de línea"
+print(my_new_line_string)
+
+my_tab_string = "\tEste es un String con tabulación"
+print(my_tab_string)
+
+my_escape_string = "\\tEste es un String \\n escapado"
+print(my_escape_string)
+
+# Formateo
+
+name, surname, age = "Yensi Jesús", "González Nova", 22
+print("Mi nombre es {} {} y mi edad es {}" .format(name, surname, age))
+print("Mi nombre es %s %s y mi edad es %s" %(name, surname,age))
+print("Mi nombre es " + name + " " + surname + " y mi edad es " + str(age))
+print(f"Mi nombre {name} {surname} y mi edad es {age}")
+
+# Desempaqueado de caracteres
+language = "python"
+a, b, c, d, e, f = language 
+print(a)
+print(e)
+
+# División 
+
+language_slice = language[1:3]
+print(language_slice)
+
+language_slice = language[1:]
+print(language_slice)
+
+# Funciones
+
+print(language.capitalize())
+print(language.upper())
+print(language.count("t"))
+print(language.isnumeric())
+print("1".isnumeric())
+print(language.lower())
+print(language.lower().isupper())
+print(language.startswith("py"))
+print("Py" == "py")
