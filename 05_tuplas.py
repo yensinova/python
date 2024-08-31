130a7a47ebc67efef1550c8cb7772d221a072792
Yensi Jesús González Nova <yensigonzalesnova55@gmail.com>
2024-08-31 15:14:55 +0200

    Tuplas

---
+++ 05_tuplas.py
@@ -0,0 +1,13 @@
+### Tuplas ###
+
+my_tuple = tuple()
+my_other_tuple = ()
+
+my_tuple = (35, 1.77, "Yensi Jesús", "González Nova", "Yensi Jesús")
+print(my_tuple)
+print(type(my_tuple))
+
+print(my_tuple[0])  # 35 
+print(my_tuple[-1]) # González Nova
+
+print(my_tuple.count("Yensi Jesús")) # Hay dos Yensi Jesús
