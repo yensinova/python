024b4456def473427f2ac1d72a35a46848484e35
Yensi Jesús González Nova <yensigonzalesnova55@gmail.com>
2024-09-01 18:44:02 +0200

    Condicionales

---
+++ 08_condicionales.py
@@ -0,0 +1,31 @@
+### CONDITIONALS ### 
+
+my_condition = True
+
+if my_condition: 
+    print("Se ejecuta la ejecución del if")
+
+my_condition = 5 * 2
+
+if my_condition > 10 and my_condition < 20: 
+    print("Es mayor que 10 y menor que 20")
+else: 
+    print("Es menor o igual que 10 0 mayor o igual que 20")
+
+
+my_string = "Mi cadena de texto"
+
+if my_string: 
+    print("Mi cadena de texto no es vacía")
+
+if my_string == "Mi cadena de texto no es vacía": 
+    print("Mi cadena de texto no es vacía")
+
+print("La ejecución continúa")
+
+if not my_string: 
+    print("Mi cadena de texto es vacía")
+
+if not my_string == "Mi cadena de texto0000000": 
+    print("Estas cadenas de texto coinciden")   
+
