62ff33ebdc09cb135b4ee95d76079b4f1cafcc73
Yensi Jesús González Nova <yensigonzalesnova55@gmail.com>
2024-09-01 19:18:48 +0200

    FUNCIONES

---
+++ 10_functions.py
@@ -0,0 +1,16 @@
+### Functions ###
+
+def my_function ():
+    print("Esto es una función")
+
+my_function ()
+my_function ()
+my_function ()
+
+def sum_two_values (first_number: int, second_number):
+    print(first_number + second_number)
+
+sum_two_values(5,7)
+sum_two_values(54344,5658)
+sum_two_values("5","7") # CONCATENA LOS NÚMEROS
+sum_two_values(5.3,7.5)
